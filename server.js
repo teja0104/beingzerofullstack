3c0c09fda51cdf3f251106a28f72ab10ed8f1b32
teja0104 <komaravolusrujana@gmail.com>
2020-01-24 12:24:19 +0530

    commited

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const path= require('path')
 
 
 app.use(express.static(__dirname+'/dist/ngproject'));
-app.get('/', (req, res) => res.sendFile(path.join(__dirnamee+'/dist/ngproject/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/dist/ngproject/index.html')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
